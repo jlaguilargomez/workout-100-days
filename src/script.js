8ed38ea7061ebab1598e386311b33cec469d5072
Jose Luis <jlaguilar90@gmail.com>
2019-09-18 17:38:06 +0200

    18sept19

---
+++ src/script.js
@@ -0,0 +1,14 @@
+let startButton = document.getElementById('start-button');
+let exitButton = document.getElementById('exit-button');
+let framework = document.getElementById('framework');
+
+
+function stage1(){
+    framework.style.display = 'none';
+
+}
+
+
+startButton.onclick= stage1;
+
+exitButton.onclick;
